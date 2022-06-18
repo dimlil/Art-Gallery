b9f13a00a3e677b595f93dd4f45d962f37ea3272
dimlil <d.v.vassilev@gmail.com>
2022-06-18 20:06:56 +0300

    feat/ added router

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import Express from "express";
+import { router } from "./router/index.js";
 
 const app = Express();
+
+app.use(router);
 
 const port = 4000;
 app.listen(port, console.log(`Server is running on port: ${port}`));
