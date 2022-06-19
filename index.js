43195ba0d487a7df81c6650e4ce7a2deeadf2bbf
dimlil <d.v.vassilev@gmail.com>
2022-06-19 23:32:31 +0300

    feat/ added handlebars

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import Express from "express";
 import { router } from "./router/index.js";
+import handlebars from 'express-handlebars'
 
 const app = Express();
 
 app.use(router);
+app.engine('.hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs'); 
+app.set('views', './views');
 
 const port = 4000;
 app.listen(port, console.log(`Server is running on port: ${port}`));
