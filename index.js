5c4163557192b201fef6a5bb4e74315221b6fa7d
dimlil <d.v.vassilev@gmail.com>
2022-06-20 16:29:51 +0300

    feat/ added statics

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ app.use(router);
 app.engine('.hbs', handlebars.engine({
     extname: '.hbs'
 }));
-app.set('view engine', '.hbs'); 
+app.set('view engine', '.hbs');
 app.set('views', './views');
+
+app.use(Express.static('static'));
 
 const port = 4000;
 app.listen(port, console.log(`Server is running on port: ${port}`));
--- router/index.js
+++ router/index.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home')
+    res.render('home',{
+        style: 'home.css'
+    })
 });
 
 export { router }
